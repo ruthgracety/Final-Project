9cdd9b37d54173b9fab22a15bbb37919b4b45929
ruthgracety <ruthgracety@gmail.com>
2024-11-03 22:58:39 +0000

    updated the apiKey

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "ccc4b1tc7of0e402eb0f24b35faed2d9";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
